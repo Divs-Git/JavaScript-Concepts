5c3f86ce0316cb05ff102800b541852b52ce1b24
Divs-Git <divyansh.sri258@gmail.com>
2024-01-09 20:53:10 +0530

    added 02_Fundamentals

---
+++ 01_Basics/01_helloWorld.js
@@ -0,0 +1,5 @@
+// Parameters: It accepts a parameter that can be an array, an object, or any message.
+// Return value: It outputs the value of the given parameter to the console.
+
+console.log('Hello Geeks');
+console.log(3 + 5);
---
+++ 02_Fundamentals/01_comparision.js
@@ -0,0 +1,19 @@
+console.log("01" == 1); // true
+console.log("apple" < "banana"); // lexographically comparision // true
+
+console.log(null == undefined); //true
+console.log(null === undefined); //false
+
+console.log(null > 0); // null becomes 0 // false
+console.log(null < 1); // true
+console.log(null >= 0); //  true
+console.log(null == 0); // null is null // false
+
+/**
+ * 3 <= 5 -> true
+ * "mango" > "banana" -> true
+ * "2" > "3" -> false
+ * undefined == null -> true
+ * null === undefined -> false
+ * null < 1 -> true
+ */
